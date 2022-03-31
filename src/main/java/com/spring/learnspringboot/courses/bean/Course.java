3c45a4a312963caf52d60fa7a61a5ee80c9014d9
udgttl12 <udgttl12@naver.com>
2022-03-31 18:11:50 +0900

    스프링 부트 공부

---
+++ src/main/java/com/spring/learnspringboot/courses/bean/Course.java
@@ -0,0 +1,34 @@
+package com.spring.learnspringboot.courses.bean;
+
+public class Course {
+    private long id;
+    private String name;
+    private String author;
+
+    public Course(long id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    @Override
+    public String toString() {
+        return "Course{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                '}';
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
---
+++ src/main/java/com/spring/learnspringboot/courses/controller/CourseController.java
@@ -0,0 +1,22 @@
+package com.spring.learnspringboot.courses.controller;
+
+import com.spring.learnspringboot.courses.bean.Course;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class CourseController {
+
+    @GetMapping("/courses")
+    public List<Course> getAllCourse(){
+        return List.of(new Course(1, "Learn Microservices", "spring"), new Course(2, "Learn Full Stack with React", "spring"));
+    }
+
+    @GetMapping("/courses/1")
+    public Course getCourseDetails(){
+        return new Course(1, "Learn Microservices", "spring");
+    }
+
+}
